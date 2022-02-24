5f12b925b83169f4084637061a1bb7229a76fce6
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-02-24 01:00:51 -0500

    Create triggerX_SF.py

---
+++ step1/triggerX/triggerX_SF.py
@@ -0,0 +1,13 @@
+import os, sys
+from argparse import ArgumentParser
+import numpy as np
+import json
+
+
+import ROOT
+
+# load the two json files
+
+# calculate the SF and SF uncertainty
+
+# print the SF in a copy-paste format for HardcodedConditions.cc
