9614de4f1681c901fdf465140426a611518e26f4
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-02-25 01:17:09 -0500

    Update step1.cc

--- step1/step1.cc
+++ step1/step1.cc
@@ -367,6 +367,9 @@ void step1::Loop(TString inTreeName, TString outTreeName, const BTagCalibrationF
   outputTree->Branch("HLT_Ele15_IsoVVVL_PFHT450",&HLT_Ele15_IsoVVVL_PFHT450,"HLT_Ele15_IsoVVVL_PFHT450/I"); //off in 17B
   outputTree->Branch("HLT_Ele28_eta2p1_WPTight_Gsf_HT150",&HLT_Ele28_eta2p1_WPTight_Gsf_HT150,"HLT_Ele28_eta2p1_WPTight_Gsf_HT150/I");
   outputTree->Branch("HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned",&HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned,"HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned/I"); 
+  outputTree->Branch("HLT_Ele15_IsoVVVL_PFHT400",&HLT_Ele15_IsoVVVL_PFHT400,"HLT_Ele15_IsoVVVL_PFHT400/I");
+  outputTree->Branch("HLT_Ele15_IsoVVVL_PFHT400_PFMET50",&HLT_Ele15_IsoVVVL_PFHT400_PFMET50,"HLT_Ele15_IsoVVVL_PFHT400_PFMET50/I");
+  outputTree->Branch("HLT_Ele50_IsoVVVL_PFHT400",&HLT_Ele50_IsoVVVL_PFHT400,"HLT_Ele50_IsoVVVL_PFHT400/I");
   // ----- mu-channel
   outputTree->Branch("HLT_Mu15_IsoVVVL_PFHT450",&HLT_Mu15_IsoVVVL_PFHT450,"HLT_Mu15_IsoVVVL_PFHT450/I");
   outputTree->Branch("HLT_PFHT400_SixJet30_DoubleBTagCSV_p056",&HLT_PFHT400_SixJet30_DoubleBTagCSV_p056,"HLT_PFHT400_SixJet30_DoubleBTagCSV_p056/I");
