2aacedd83ff847ca0293020f785ce5ec0995921b
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-04-04 14:17:59 -0400

    Update config.py

--- step2/config.py
+++ step2/config.py
@@ -16,9 +16,10 @@ step2Path = {
   } for year in years
 }
 
+# https://docs.google.com/presentation/d/1gEF2jExqS1hWs-dlYgxrTT2xQztYbBZcrLdw45Aiy7g/edit#slide=id.ge8cc301637_0_55
 lumi = {
-  "16APV": 20710., # preVFP ratio (57%) calculated using brilcalc on ReReco but total lumi from GoldenJSON
-  "16": 15623.,    # postVFP ratio (43%) calculated using brilcalc on ReReco but total lumi from GoldenJSON
+  "16APV": 19520., # From PdmV 
+  "16": 16810.,    # From PdmV 
   "17": 41480.,    # calculated using brilcalc on GoldenJSON 
   "18": 59832.     # calculated using brilcalc on GoldenJSON
 }
